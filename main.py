b268c3611f4b044386e8fc11e0c8916a4d4784d1
nitya <nityaoberoi09@gmail.com>
2024-01-18 00:37:05 +0530

    day 30: return temp for for any station and date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app=Flask(__name__)
 @app.route("/")
@@ -7,7 +9,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")#these two denote that user can enter its value
 def about(station,date):
-    temprature=23
+    filename="small data/TG_STAID"+str(station).zfill(6)+".txt" # zfills add 0s' to the string such as 1 will be written as 000001
+    df=pd.read_csv(filename,skiprows=20, parse_dates=["    DATE"])
+    temprature= df.loc[df["    DATE"]==date]["   TG"].squeeze()/10
     return {"station":station,"date":date, "temprature":temprature}
 
 
