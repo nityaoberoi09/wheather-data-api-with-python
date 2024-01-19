1a266562fe17aef28c77e42979e2b0fdcdbb4bf1
nitya <nityaoberoi09@gmail.com>
2024-01-19 13:12:54 +0530

    day 31: add url endpoint for yearly data

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ def about(station,date):
     temprature= df.loc[df["    DATE"]==date]["   TG"].squeeze()/10
     return {"station":station,"date":date, "temprature":temprature}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "small data/TG_STAID" + str(station).zfill(6) + ".txt"  # zfills add 0s' to the string such as 1 will be written as 000001
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result=df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "small data/TG_STAID" + str(station).zfill(6) + ".txt"  # zfills add 0s' to the string such as 1 will be written as 000001
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"]=df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 
 if __name__=="__main__":
     app.run(debug=True)
